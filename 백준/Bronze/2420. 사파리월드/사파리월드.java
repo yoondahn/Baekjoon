3a544c28c5634f51a5cbde93f8c5490d21d1429e
YoonDaHyun <101653672+yoondahn@users.noreply.github.com>
2024-07-06 16:12:07 +0900

    [Bronze V] Title: 사파리월드, Time: 208 ms, Memory: 17540 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long N = sc.nextLong();
+        long M = sc.nextLong();
+
+        System.out.print(Math.abs(N-M));
+    }
+}
