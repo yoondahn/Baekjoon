bc7ff8e7fe7d5b854899d4b85a71b3268661285e
YoonDaHyun <101653672+yoondahn@users.noreply.github.com>
2024-07-07 16:07:13 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ascii = sc.next().charAt(0);
+        System.out.println(ascii);
+    }
+}
