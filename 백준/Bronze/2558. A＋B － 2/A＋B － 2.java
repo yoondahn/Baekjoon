806a6e07dacaf44bbfa8e6b92ca7e6fe6be6468f
YoonDaHyun <101653672+yoondahn@users.noreply.github.com>
2024-07-14 01:16:05 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        sb.append((char)a+b);
+        System.out.println(sb);
+    }
+}
