f18ada811d61495faa16a6b50461e38c68313544
YoonDaHyun <101653672+yoondahn@users.noreply.github.com>
2024-08-09 01:24:26 +0900

    [Bronze V] Title: 아!, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        sc.close();
+        if(str1.length() >= str2.length()){
+            System.out.println("go");
+            //(1)
+        }else{
+            System.out.println("no");
+            //(2)
+        }
+    }
+}
